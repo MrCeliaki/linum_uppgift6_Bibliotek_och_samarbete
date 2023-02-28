fc55f2fe2e2bef470df117c2052a212699400180
Robert Freiholtz <robert.freiholtz@outlook.com>
2023-02-28 22:04:51 +0100

    power.* dummy inplace

--- power.cpp
+++ power.cpp
@@ -1,1 +1,14 @@
 #include "power.h"
+
+namespace Power
+{
+    float Power::calc_power_r(float volt, float resistance)
+    {
+        return 0.0f;
+    }
+
+    float Power::calc_power_i(float volt, float current)
+    {
+        return 0.0f;
+    } 
+}
--- power.h
+++ power.h
@@ -1,2 +1,11 @@
-float calc_power_r(float volt, float resistance);
-float calc_power_i(float volt, float current);
+#pragma once
+
+namespace Power
+{
+    class Power
+    {
+        public:
+            float calc_power_r(float volt, float resistance);
+            float calc_power_i(float volt, float current);
+    }
+}
